25a719777dfd439c9dd8e2e8b6b1bb077e216be8
Alain Cajuste <cajuste.alain@gmail.com>
2014-11-06 18:16:31 +0100

    Fix game

--- src/js/gamememory.js
+++ src/js/gamememory.js
@@ -21,15 +21,15 @@
         plugin.construct = function() {
             plugin.settings = $.extend({}, defaults, options);
             $element.append('<h1 class="title-game text-center">Jeu du memory</h1>');
-            $element.append('<section class="row"><div id="tiling" class="col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 tiling"></div></section>');
+            $element.append('<section class="row"><button class="button-replay" onClick="window.location.reload()"><span>Rejouer</span> <i class="fa fa-refresh"></i></button><div id="tiling" class="col-lg-10 col-lg-offset-1 tiling"></div></section>');
             var $tiling = $element.find('#tiling');
             if (plugin.settings.contents.length == 0) {
-                $tiling.append('<p class="text-center">Pas de carte</p>');
+                $tiling.append('<p class="text-center">Pas de tuiles</p>');
             } else {
                 tiling = _.sample(plugin.settings.contents, plugin.settings.quantity / 2);
                 tiling = _.shuffle($.merge(tiling, tiling));
                 for (var i = 0; i <= tiling.length - 1; i++) {
-                    $tiling.append('<li class="tile col-xs-5 col-sm-2 col-md-4 col-lg-2"><div class="front"><i class="fa fa-eye"></i></div>' +
+                    $tiling.append('<li class="tile col-lg-2"><div class="front"><i class="fa fa-eye"></i></div>' +
                         tiling[i]["content"] + '</li>');
                 };
             }
@@ -45,29 +45,25 @@
         var setClickTile = function() {
             $element.find('.tile').on('click', function(e) {
                 var index = $(this).index();
-                if (_.isEmpty(currentTile)) {
+                if (currentIndex != index && nbTileLook < 2) {
+                    currentIndex = index;
                     currentTile.push({
                         id: $(this).find('.back').attr("data-tile"),
                         index: index
                     });
-                }
-
-                if (currentIndex != index) {
-                    currentIndex = index;
                     if (nbTileLook != 2) {
                         nbClick++;
                         nbTileLook++;
                         $element.find('#number-click').text(nbClick);
                         $(this).toggleClass("tile-look");
                         timerId.push(_.delay(onTimeOutHideTile($(this)), plugin.settings.interval));
-                        if (nbTileLook == 2 && currentTile[0].index != index) {
-                            currentTile.push({
-                                id: $(this).find('.back').attr("data-tile"),
-                                index: index
-                            });
+                        console.log("nbTileLook : " + nbTileLook);
+                        if (nbTileLook == 2) {
                             onMatchedTile();
                         }
                     }
+                } else {
+                    toastr.info('Dèjà active !');
                 }
             });
         }
@@ -77,15 +73,21 @@
                     e.toggleClass("tile-look");
                     currentIndex = -1;
                     nbTileLook--;
+                    currentTile = [];
                 }
             }
         }
         var onMatchedTile = function() {
+            toastr.clear();
             currentIndex = -1;
             if (currentTile[0].id == currentTile[1].id) {
                 toastr.success('Win !');
                 $element.find('.back[data-tile="' + currentTile[0].id + '"]').parent().addClass('tile-valid');
                 currentTile = [];
+                for (var i = 0; i < timerId.length; i++) {
+                    clearTimeout(timerId[i]);
+                };
+                nbTileLook = 0;
             } else {
                 toastr.error('Loser !');
                 currentTile = [];
@@ -93,15 +95,15 @@
                     clearTimeout(timerId[i]);
                 };
                 _.delay(function() {
+                    nbTileLook = 0;
                     $element.find(".tile").each(function() {
                         if ($(this).hasClass("tile-look")) {
                             $(this).removeClass("tile-look");
-                            nbTileLook--;
                         }
                     });
                 }, 1000);
-
             }
+            timerId = [];
         }
 
         /*** external api ***/
